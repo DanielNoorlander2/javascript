e5b852d14d9ce68a6c346f356a7db3c7342c6aff
DanielNoorlander2 <43062214+DanielNoorlander2@users.noreply.github.com>
2018-12-03 11:57:41 +0000

    Add files via upload
    JavaScript DOM

---
+++ vulmetjs.js
@@ -0,0 +1,11 @@
+
+
+
+
+
+document.getElementById("paginatitel").innerText = "Gave website";
+document.getElementById("subkop").innerText = "En nog een subkop";
+document.getElementById("aap1").src = "img/aap1.jpg"
+document.getElementById("aap2").src = "img/aap2.jpg";
+
+
